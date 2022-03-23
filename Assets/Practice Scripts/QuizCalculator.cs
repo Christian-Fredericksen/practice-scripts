bdaee991cccae11ef5a203a8243f4f28a5f49f07
Christian Fredericksen <christian.fredericksen@yahoo.com>
2022-03-23 08:42:49 -0700

    finished Quiz Calculator

--- Assets/Practice Scripts/QuizCalculator.cs
+++ Assets/Practice Scripts/QuizCalculator.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 public class QuizCalculator : MonoBehaviour
 {
     //Should calculate the average of 5 quiz grades
+
     //Should randomly generate a grade for each quiz at start up
 
     //Need to create variables to store the value of the grades
     //Need to create a variable to stor the average
-    
-
-    public float quiz1 = Random.Range(0.0f, 100.0f);
-    public float quiz2 = Random.Range(0.0f, 100.0f);
-    public float quiz3 = Random.Range(0.0f, 100.0f);
-    public float quiz4 = Random.Range(0.0f, 100.0f);
-    public float quiz5 = Random.Range(0.0f, 100.0f);
+    //int num = Random.Range(0, 100);
+    public int quiz1;
+    public int quiz2;
+    public int quiz3;
+    public int quiz4;
+    public int quiz5;
 
     public float average;
     // Start is called before the first frame update
     void Start()
     {
-        
-        float totalPoints = quiz1 + quiz2 + quiz3 + quiz4 + quiz5;
-        float average = totalPoints / 100f;
+        quiz1 = Random.Range(0, 101);
+        quiz2 = Random.Range(0, 101);
+        quiz3 = Random.Range(0, 101);
+        quiz4 = Random.Range(0, 101);
+        quiz5 = Random.Range(0, 101);
+        int totalPoints = quiz1 + quiz2 + quiz3 + quiz4 + quiz5;
+        average = totalPoints / 5f;
         float gradeAverage = (float)System.Math.Round(average * 100f) / 100f;
         Debug.Log("Your GPA is: " + gradeAverage);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
